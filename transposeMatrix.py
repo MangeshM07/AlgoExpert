92a68016b32b12fc580c93fc99da98b20705fd42
MangeshM07 <mangesh.mankar04@gmail.com>
2023-10-19 16:46:59 +0530

    Added BST

---
+++ transposeMatrix.py
@@ -0,0 +1,17 @@
+def transposeMatrix(matrix):
+    transposedMatrix = []
+
+    for col in range(len(matrix[0])):
+        newRow = []
+        for row in range(len(matrix)):
+            newRow.append(matrix[row][col])
+        transposedMatrix.append(newRow)
+    return transposedMatrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]
+
+print(transposeMatrix(matrix))
