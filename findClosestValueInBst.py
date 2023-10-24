09ff139748a64adab77bf1f79acff4112d6f8e29
MangeshM07 <mangesh.mankar04@gmail.com>
2023-10-24 21:29:06 +0530

    find closest value to the given value in BST

--- findClosestValueInBst.py
+++ findClosestValueInBst.py
@@ -1,1 +1,37 @@
+#           Time    | Space
+# Average: O(log n) | O(log n)
+# Worst:       O(n) | O(n)
 def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, float("inf"))
+
+# Recursive approach
+def findClosestValueInBstHelper(tree, target, closest):
+    if tree is None:
+        return closest
+
+    if abs(target-closest) > abs(target - tree.value):
+        closest = tree.value
+
+    if target < tree.value:
+        return findClosestValueInBstHelper(tree.left, target, closest)
+    elif target > tree.value:
+        return findClosestValueInBstHelper(tree.right, target, closest)
+    else:
+        return closest
+
+# Iterative  approach
+# Average:          O(log n) Time   | O(log n)  Space
+# Worst:             O(n) Time      | O(n)  Space
+def findClosestValueInBstHelper(tree, target, closest):
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target-closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        elif target > currentNode.value:
+            currentNode = currentNode.right
+        else:
+            break
+    return closest
