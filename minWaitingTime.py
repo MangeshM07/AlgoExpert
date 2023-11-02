5c49ec198a6876866ac91bb2c26073a6a4d98aed
MangeshM07 <mangesh.mankar04@gmail.com>
2023-11-02 17:55:28 +0530

    min waiting time

---
+++ minWaitingTime.py
@@ -0,0 +1,15 @@
+# Time O(n log n) | space O(1) time
+
+def minWaitingTime(queries):
+    queries.sort()
+
+    totalWaitingTime = 0
+
+    for idx, duration in enumerate(queries):
+        queriesLeft = len(queries) - (idx +1)
+        totalWaitingTime += duration * queriesLeft
+
+    return totalWaitingTime
+
+q = [3,2,1,2,6]
+print(minWaitingTime(q))
