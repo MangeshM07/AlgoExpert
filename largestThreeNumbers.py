c9cad86e6eae16747c372229fd9c0700bfc6e203
MangeshM07 <mangesh.mankar04@gmail.com>
2024-01-04 06:35:20 +0530

    find largest 3 numbers in unsorted array

--- largestThreeNumbers.py
+++ largestThreeNumbers.py
@@ -1,6 +1,6 @@
-# def largestThreeNumbers_solution1(array):
-#     newArr = sorted(array)
-#     return newArr[-3:]
+def largestThreeNumbers_solution1(array):
+    newArr = sorted(array)
+    return newArr[-3:]
 
 
 def largestThreeNumbers_solution2(array):
@@ -27,5 +27,30 @@ def shiftAndUpdate(array, num, idx):
             array[i] = array[i + 1]
 
 
-arr = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
-print(largestThreeNumbers_solution2(arr))
+def largestThreeNumbers_solution3(array):
+    if len(array) < 3:
+        return "Array should have at least three elements."
+
+    first = second = third = float('-inf')
+
+    for num in array:
+        if num > first:
+            third = second
+            second = first
+            first = num
+        elif num > second:
+            third = second
+            second = num
+        elif num > third:
+            third = num
+
+    return [third, second, first]
+
+# Example usage:
+array = [10, 5, 2, 9, 12, 7, 17, 4]
+result = largestThreeNumbers_solution3(array)
+print(result)
+
+
+arr = [141, 1, 17, -7, -17, -27, 18, 541, 541, 8, 7, 7]
+print(largestThreeNumbers_solution3(arr))
