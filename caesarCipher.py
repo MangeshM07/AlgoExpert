fe79d58bcbdae6cd6f651d264ff6ab5cdef188da
MangeshM07 <mangesh.mankar04@gmail.com>
2024-01-06 08:02:03 +0530

    Ceasar Cipher encryptor

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,11 +1,40 @@
-def ceasarCipher(string, key):
+def ceasarCipherEncryptor_solution1(string, key):
     newString = ""
     for i in range(len(string)):
         nlc = ord(string[i]) + key
         if nlc > 122:
-            newString+=chr(96+(nlc%122))
+            newString += chr(96 + (nlc % 122))
         else:
-            newString+=chr(nlc)
+            newString += chr(nlc)
     return newString
 
-print(ceasarCipher('xyz',2))
+
+def ceasarCipherEncryptor_solution2(string, key):
+    newLetters = []
+    newKey = key % 26
+
+    for letter in string:
+        newLetters.append(getNewLetter_solution2(letter, newKey))
+    return "".join(newLetters)
+
+
+def getNewLetter_solution2(Letter, key):
+    newLetterCode = ord(Letter) + key
+    return chr(newLetterCode) if newLetterCode <= 122 else chr(96 + newLetterCode % 122)
+
+
+def ceasarCipherEncryptor_solution3(string, key):
+    newLetters = []
+    newKey = key % 26
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+    for letter in string:
+        newLetters.append(getNewLetter_solution3(letter, newKey, alphabet))
+    return "".join(newLetters)
+
+
+def getNewLetter_solution3(letter, key, alphabet):
+    newLetterCode = alphabet.index(letter) + key
+    return alphabet[newLetterCode] if newLetterCode < 25 else alphabet[newLetterCode % 26]
+
+
+print(ceasarCipherEncryptor_solution3('xyz', 2))
