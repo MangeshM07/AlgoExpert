59a37c4a3172c4b9982e3e15e3ba70eee076986b
MangeshM07 <mangesh.mankar04@gmail.com>
2024-01-10 21:44:19 +0530

    first non repeating character

--- firstNonRepeatingCharacter.py
+++ firstNonRepeatingCharacter.py
@@ -1,13 +1,43 @@
+# Time O(n^2) | space O(n)
 def firstNonRepeatingCharacter(string):
     seen_chars = set()
 
     for index, char in enumerate(string):
         if char not in seen_chars:
-            if char not in string[index+1:]:
+            if char not in string[index + 1:]:
                 return index
         seen_chars.add(char)
 
     return -1
 
+
+def firstNonRepeatingCharacter_solution2(string):
+    for idx in range(len(string)):
+        foundDuplicate = False
+        for idx2 in range(len(string)):
+            if string[idx] == string[idx2] and idx != idx2:
+                foundDuplicate = True
+
+        if not foundDuplicate:
+            return idx
+
+    return -1
+
+
+# O(n) time | O(1) space
+def firstNonRepeatingCharacter_solution3(string):
+    characterFrequencies = {}
+
+    for character in string:
+        characterFrequencies[character] = characterFrequencies.get(character, 0) + 1
+
+    for idx in range(len(string)):
+        character = string[idx]
+        if characterFrequencies[character] == 1:
+            return idx
+
+    return -1
+
+
 string = "abcdcaf"
-print(firstNonRepeatingCharacter(string))
+print(firstNonRepeatingCharacter_solution3(string))
