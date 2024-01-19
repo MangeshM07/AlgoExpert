b8697235469ab2aa5fcb75cd9efa428d9ed5d4d4
MangeshM07 <mangesh.mankar04@gmail.com>
2024-01-19 06:58:35 +0530

    array of products

---
+++ arrayOfProducts.py
@@ -0,0 +1,51 @@
+def arrayOfProducts_sol1(array):
+    result = []
+
+    for i in array:
+        product = 1
+        for j in array:
+            if i != j:
+                product = product * j
+        result.append(product)
+    return result
+
+
+def arrayOfProducts_sol2(array):
+    products = [1 for _ in range(len(array))]
+    leftProducts = [1 for _ in range(len(array))]
+    rightProducts = [1 for _ in range(len(array))]
+
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        leftProducts[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        rightProducts[i] = rightRunningProduct
+        rightRunningProduct *= array[i]
+
+    for i in range(len(array)):
+        products[i] = leftProducts[i] * rightProducts[i]
+
+    return products
+
+
+def arrayOfProducts_sol3(array):
+    products = [1 for _ in range(len(array))]
+
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        products[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        products[i] *= rightRunningProduct
+        rightRunningProduct *= array[i]
+
+    return products
+
+
+array = [9, 3, 2, 1, 9, 5, 3, 2]
+print(arrayOfProducts_sol3(array))
