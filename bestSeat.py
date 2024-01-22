5673406eacceaf70d8da4bc8f95846c9afd7ff49
MangeshM07 <mangesh.mankar04@gmail.com>
2024-01-22 21:10:33 +0530

    best seat in an array

---
+++ bestSeat.py
@@ -0,0 +1,18 @@
+def bestSeat(seats):
+    bestSeat = -1
+    maxSpace = 0
+
+    leftPointer = 0
+    while leftPointer < len(seats):
+        rightPointer = leftPointer + 1
+        while rightPointer < len(seats) and seats[rightPointer] == 0:
+                rightPointer += 1
+
+        availableSpace = rightPointer - leftPointer - 1
+        if availableSpace > maxSpace:
+            bestSeat = (rightPointer + leftPointer) // 2
+            maxSpace = availableSpace
+        leftPointer = rightPointer
+
+    return bestSeat
+
