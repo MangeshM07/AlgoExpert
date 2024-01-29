d3f78901b5913c1079b320a0652e314f906981b9
MangeshM07 <mangesh.mankar04@gmail.com>
2024-01-29 21:02:54 +0530

    Kth largest value in BST

---
+++ kthLargestValueInBST_solution2.py
@@ -0,0 +1,27 @@
+class BST:
+    def __init__(self, value, left = None, right =None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class TreeInfo:
+    def __init__(self, numberOfNodesVisited, latestVisitedNodeValue):
+        self.numberOfNodesVisited = numberOfNodesVisited
+        self.latestVisitedNodeValue = latestVisitedNodeValue
+
+# O(h+k) time | O(h) space
+def findKthLargestValueInBst(tree, k):
+    treeInfo = TreeInfo(0, -1)
+    reverseInOrderTraverse(tree, k , treeInfo)
+    return treeInfo.latestVisitedNodeValue
+
+def reverseInOrderTraverse(node, k, treeInfo):
+    if node == None or treeInfo.numberOfNodesVisited >= k:
+        return
+
+    reverseInOrderTraverse(node.right, k , treeInfo)
+    if treeInfo.numberOfNodesVisited < k:
+        treeInfo.numberOfNodesVisited += 1
+        treeInfo.latestVisitedNodeValue = node.value
+        reverseInOrderTraverse(node.left, k , treeInfo)
+
