c17a5c7de1750996659cc9b3251f5601415ed5de
MangeshM07 <mangesh.mankar04@gmail.com>
2024-02-08 16:21:14 +0530

    Can Binary tree be split into 2 equal half

---
+++ splitBinaryTree.py
@@ -0,0 +1,29 @@
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+# O(n) time | O(h) space - where n is the number of nodes in the tree
+# and h is the height of the tree
+def splitBinaryTree(tree):
+    desiredSubtreeSum = getTreeSum(tree)/2
+    canBeSplit = trySubtrees(tree, desiredSubtreeSum)[1]
+    return desiredSubtreeSum if canBeSplit else 0
+
+def trySubtrees(tree, desiredSubtreeSum):
+    if tree is None:
+        return (0, False)
+
+    leftSum, leftCanBeSplit = trySubtrees(tree.left, desiredSubtreeSum)
+    rightSum, rightCanBeSplit = trySubtrees(tree.right, desiredSubtreeSum)
+
+    currentTreeSum = tree.value + leftSum + rightSum
+    canBeSplit = leftCanBeSplit or rightCanBeSplit or currentTreeSum == desiredSubtreeSum
+    return (currentTreeSum, canBeSplit)
+
+def getTreeSum(tree):
+    if tree is None:
+        return 0
+    return tree.value + getTreeSum(tree.left) + getTreeSum(tree.right)
+
