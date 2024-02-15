4893b67a0660da1153011ad426e2ce9d8c7a365c
MangeshM07 <mangesh.mankar04@gmail.com>
2024-02-15 21:38:06 +0530

    minimum number of coins required for change

---
+++ reverseWordsInString.py
@@ -0,0 +1,17 @@
+def reverseWordsInString(string):
+    output = []
+
+    left = right = len(string)-1
+
+    while left >= 0 and right >= 0:
+        if string[left] != ' ':
+            left = left - 1
+        else:
+            output.append(string[left + 1:right + 1])
+            left = right = left - 1
+    output.append(string[left+1:right+1])
+    return " ".join(output)
+
+
+input = "Algoexpert  is the best!!"
+print(reverseWordsInString(input))
