5f44d6d6cadf7b77742358be8cb620b7c7f91bfe
MangeshM07 <mangesh.mankar04@gmail.com>
2024-02-21 18:01:31 +0530

    kadences algorithm

--- kadanesAlgorithm.py
+++ kadanesAlgorithm.py
@@ -1,1 +1,12 @@
+# O(n) time | O(n) space
 def kadanesAlgorithm(array):
+    maxEndingHere = array[0]
+    maxSoFar = array[0]
+    for i in range(1, len(array)):
+        num = array[i]
+        maxEndingHere = max(num, maxEndingHere + num)
+        maxSoFar = max(maxSoFar, maxEndingHere)
+    return maxSoFar
+
+a =[3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+print(kadanesAlgorithm(a))
