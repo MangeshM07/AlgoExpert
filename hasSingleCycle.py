c29719eba6f523c295540935e6cf2e362b8dc143
MangeshM07 <49366841+MangeshM07@users.noreply.github.com>
2024-02-26 22:43:59 +0530

    has single cycle

---
+++ hasSingleCycle.py
@@ -0,0 +1,21 @@
+# O(n) time | O(1) space
+def hasSingleCycle(array):
+    numElementVisited = 0
+    currentIdx = 0
+
+    while numElementVisited < len(array):
+        if numElementVisited > 0 and currentIdx == 0:
+            return False
+        numElementVisited += 1
+        currentIdx = getNextIdx(currentIdx, array)
+    return currentIdx == 0
+
+
+def getNextIdx(currentIdx, array):
+    jump = array[currentIdx]
+    nextIdx = (currentIdx + jump) % len(array)
+    return nextIdx if nextIdx >= 0 else nextIdx + len(array)
+
+
+arr = [2,3,1,-4,-4,2]
+print(hasSingleCycle(arr))
