519c0f012a71500d3f8c5da1a38ce4485cebf831
MangeshM07 <mangesh.mankar04@gmail.com>
2024-03-07 14:01:49 +0530

    cycle in graph solution 2

---
+++ cycleInGraph_sol2.py
@@ -0,0 +1,40 @@
+# O(v+e) time | O(v) space
+
+
+WHITE, GREY, BLACK = 0, 1, 2
+# WHITE means unvisited
+# GREY means visited but not fully processed, still in recursive stack
+# BLACK fully processed
+def cycleInGraph(edges):
+    numberOfNodes = len(edges)
+    colors = [WHITE for _ in range(numberOfNodes)]
+
+    for node in range(numberOfNodes):
+        if colors[node] != WHITE
+            continue
+
+        containsCycle = traverseAndColorNodes(node, edges, colors)
+        if  containsCycle:
+            return True
+
+    return False
+
+def traverseAndColorNodes(node, edges, colors):
+    colors[node] = GREY
+
+    neighbors = edges[node]
+    for neighbor in neighbors:
+        neighborColor = colors[neighbor]
+
+        if neighborColor == GREY:
+            return True
+
+        if neighborColor != WHITE:
+            continue
+
+        containsCycle = traverseAndColorNodes(neighbor, edges, colors)
+        if containsCycle:
+            return True
+
+    colors[node] = BLACK
+    return False
