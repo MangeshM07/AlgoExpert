0fdfa9fa478f98851be8c9255ccd5d7af927c36c
MangeshM07 <mangesh.mankar04@gmail.com>
2024-03-13 20:33:06 +0530

    task assignment

--- taskAssignments.py
+++ taskAssignments.py
@@ -1,1 +1,36 @@
-def taskAssignments(k, tasks):
+def taskAssignment(k, tasks):
+    pairedTasks = []
+    taskDurationsToIndices = getTaskDurationToIndices(tasks)
+
+    sortedTasks = sorted(tasks)
+    for idx in range(k):
+        task1Duration = sortedTasks[idx]
+        indicesWithTask1Duration = taskDurationsToIndices[task1Duration]
+        task1Index = indicesWithTask1Duration.pop()
+
+        task2SortedIndex = len(tasks) - 1 - idx
+        task2Duration = sortedTasks[task2SortedIndex]
+        indicesWithTask2Duration = taskDurationsToIndices[task2Duration]
+        task2Index = indicesWithTask2Duration.pop()
+
+        pairedTasks.append([task1Index, task2Index])
+    return pairedTasks
+
+
+def getTaskDurationToIndices(tasks):
+    taskDurationsToIndices = {}
+
+    for idx, taskDuration in enumerate(tasks):
+        if taskDuration in taskDurationsToIndices:
+            taskDurationsToIndices[taskDuration].append(idx)
+        else:
+            taskDurationsToIndices[taskDuration] = [idx]
+
+    return taskDurationsToIndices
+
+
+k = 3,
+tasks = [1, 3, 5, 3, 1, 4]
+output = taskAssignment(k, tasks)
+# print(output)
+
