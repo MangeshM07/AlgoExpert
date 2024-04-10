78059061ab852c9b5b660019a21755e055bc2183
MangeshM07 <mangesh.mankar04@gmail.com>
2024-04-10 17:49:06 +0530

    Doubly Linked List

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -29,8 +29,22 @@ class DoublyLinkedList:
 
 
     def remove(self, node):
-        pass
+        if node == self.head:
+            self.head = self.head.next
+        if node == self.tail:
+            self.tail = self.tail.prev
+        self.removeNodeBindings(node)
 
     def containsNodeWithValue(self, value):
-        pass
+        node = self.head
+        while node is not None and node.value != value:
+            node = node.next
+        return node is not None
 
+    def removeNodeBindings(self, node):
+        if node.prev is not None:
+            node.prev.next = node.next
+        if node.next is not None:
+            node.next.prev = node.prev
+        node.prev = None
+        node.next = None
