df5c5683d3a2c70cf45866b045425f8ef407284d
MangeshM07 <mangesh.mankar04@gmail.com>
2024-04-15 20:31:25 +0530

    remove nth node from linked list

---
+++ sumOfLinkedLists.py
@@ -0,0 +1,13 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def sumOfLinkedLists(linkedListOne, linkedListTwo):
+    # Write your code here.
+    return linkedListOne
+
+
+
