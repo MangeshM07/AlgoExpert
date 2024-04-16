c75728f57c50ee286f3458965b25a44156ed49b7
MangeshM07 <mangesh.mankar04@gmail.com>
2024-04-16 20:40:10 +0530

    Sum of linked list

--- sumOfLinkedLists.py
+++ sumOfLinkedLists.py
@@ -6,9 +6,26 @@ class LinkedList:
 
 
 def sumOfLinkedLists(linkedListOne, linkedListTwo):
-    # Write your code here.
+    newLinkedListHeadPointer = LinkedList(0)
+    currentNode = newLinkedListHeadPointer
+    carry = 0
 
-    return linkedListOne
+    nodeOne = linkedListOne
+    nodeTwo = linkedListTwo
+    while nodeOne is not None or nodeTwo is not None or carry != 0:
+        valueOne = nodeOne.value if nodeOne is not None else 0
+        valueTwo = nodeTwo.value if nodeTwo is not None else 0
+        sumOfValues = valueOne + valueTwo + carry
 
+        newValue = sumOfValues % 10
+        newNode = LinkedList(newValue)
+        currentNode.next = newNode
+        currentNode = newNode
+
+        carry = sumOfValues // 10
+        nodeOne = nodeOne.next if nodeOne is not None else None
+        nodeTwo = nodeTwo.next if nodeTwo is not None else None
+
+    return newLinkedListHeadPointer.next
 
 
---
+++ test_sumOfLinkedList.py
@@ -0,0 +1,31 @@
+import sumOfLinkedLists
+import unittest
+
+
+class LinkedList(sumOfLinkedLists.LinkedList):
+    def addMany(self, values):
+        current = self
+        while current.next is not None:
+            current = current.next
+        for value in values:
+            current.next = LinkedList(value)
+            current = current.next
+        return self
+
+
+def getNodesInArray(output):
+    nodes = []
+    current = output
+    while current is not None:
+        nodes.append(current.value)
+        current = current.next
+    return nodes
+
+
+class TestsumOfLinkedLists(unittest.TestCase):
+    def test_case_1(self):
+        ll1 = LinkedList(2).addMany([4, 7, 1])
+        ll2 = LinkedList(9).addMany([4, 5])
+        expected = LinkedList(1).addMany([9, 2, 2])
+        actual = sumOfLinkedLists.sumOfLinkedLists(ll1, ll2)
+        self.assertEqual(getNodesInArray(actual), getNodesInArray(expected))
