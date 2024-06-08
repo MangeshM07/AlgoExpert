2400f764c3b6dbe20161b1e305c3fec3bf9a21e7
MangeshM07 <mangesh.mankar04@gmail.com>
2024-06-08 20:32:38 +0530

    climbing stairs problem

--- staircaseTraversal.py
+++ staircaseTraversal.py
@@ -1,2 +1,14 @@
-def staircaseTraversal(height, maxSteps):
-    return -1
+def climbingStairs(noOfSteps):
+    if noOfSteps == 1:
+        return 1
+    elif noOfSteps == 2:
+        return 2
+    else:
+        return climbingStairs(noOfSteps - 2) + climbingStairs(noOfSteps - 1)
+
+
+noOfSteps = 8
+print(climbingStairs(noOfSteps))
+
+
+
